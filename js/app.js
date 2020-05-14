852e33ed6303571d056a24ec8332ccdb5019c4bc
Emiliano Vilche <31260215+Doomlash@users.noreply.github.com>
2020-05-14 16:46:59 -0300

    Timer implementado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+var playerText = document.getElementById('playerText');
+var referenceText = document.getElementById('referenceText').innerHTML;
+playerText.maxLength=referenceText.length;
+playerText.value = '';
 
+var startingTime;
+var characterCounter = 0;
+var timer=document.getElementById('timer');
+
+playerText.addEventListener("keydown", checkInput);
+
+function checkInput(keyPressed){
+	var playerTextContent = playerText.value;
+	if(playerTextContent.length==0&&timer.innerHTML=="0:00.000"){startTimer();}
+}
+
+function startTimer(){
+	startingTime= new Date();
+	writingTimer = setInterval(timerRunning, 10);
+}
+function stopTimer(){
+	clearInterval(writingTimer);
+}
+function timerRunning(){
+	var currentTime = new Date();
+	var timeElapsed= new Date(currentTime-startingTime);
+	var min = timeElapsed.getUTCMinutes()
+    var sec = timeElapsed.getUTCSeconds()
+    var ms = timeElapsed.getUTCMilliseconds();
+	timer.innerHTML=
+		min + ":" + 
+        (sec > 9 ? sec : "0" + sec) + "." + 
+        (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+}
